3722a576425ae60d1eb9a396adb1a6cf87cf0ff3
Hristina Adamovic <hristinacina@gmail.com>
2024-05-12 12:07:08 +0200

    created event classes

--- backend/model/src/main/java/com/ftn/sbnz/model/events/FinancialAid.java
+++ backend/model/src/main/java/com/ftn/sbnz/model/events/FinancialAid.java
@@ -1,5 +1,21 @@
 package com.ftn.sbnz.model.events;
 
+import java.util.Date;
+
+import com.ftn.sbnz.model.models.Requirement;
+import org.kie.api.definition.type.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Role(Role.Type.EVENT)
+//TODO definisati ovaj datum
 public class FinancialAid {
-    
+    private String type;
+    private double amount;
+    private Requirement requrement;
+    private Date deadline;
 }
--- backend/model/src/main/java/com/ftn/sbnz/model/events/InterestChange.java
+++ backend/model/src/main/java/com/ftn/sbnz/model/events/InterestChange.java
@@ -1,5 +1,19 @@
 package com.ftn.sbnz.model.events;
 
+import java.util.Date;
+import java.util.List;
+
+import org.kie.api.definition.type.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Role(Role.Type.EVENT)
 public class InterestChange {
-    
+    private List<String> oldInterests;
+    private List<String> currentInterests;
+    private Date timestamp;
 }
